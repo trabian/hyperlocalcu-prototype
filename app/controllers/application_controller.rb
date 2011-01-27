2cb51bbe6c794a6d9082492363cfc5691473c30e
Matt Dean <matt@trabian.com>
2011-01-27 10:05:03 -0500

    Fix problem with 304 status on Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :add_expires_header
+
+  def add_expires_header
+    expires_in 0, :public => true
+  end
+
 end
