073843b41864847658b1ef1e6a87e05544dc62b8
Matt Dean <matt@trabian.com>
2011-02-09 02:16:16 -0500

    Avoid double-fetching events

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,7 +6,7 @@ class AccountsController < ApplicationController
 
   def index
 
-    redirect_to account_events_path(current_user.member.accounts.first)
+    redirect_to account_path(current_user.member.accounts.first)
     
   end
 
